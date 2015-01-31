04cacfb12398869164114899c875c6637a7b6c61
Shane Aston <astonshane@gmail.com>
2015-01-31 17:04:12 -0500

    made /subs/ restricted to logged in users

--- alienspoilers/home/views.py
+++ alienspoilers/home/views.py
@@ -5,7 +5,9 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
+    if request.user.is_authenticated():
+        return render(request, 'subs/index.html')
+    else:
         return render(request,
-                'home/index.html',
-                {} )
+                'home/index.html')
     #return HttpResponse("Hello World. Homepage")
--- alienspoilers/subs/views.py
+++ alienspoilers/subs/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from subs.forms import UserForm, UserProfileForm
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
+@login_required
 def index(request):
     return render(request,
             'subs/index.html')
