501b7c6fcdf18bd6a14cf9b013ade00098c17140
Shane Aston <astonshane@gmail.com>
2015-01-31 17:10:05 -0500

    added logout page / logic

--- alienspoilers/subs/views.py
+++ alienspoilers/subs/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from subs.forms import UserForm, UserProfileForm
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 
 @login_required
@@ -102,3 +103,15 @@ def user_login(request):
         # No context variables to pass to the template system, hence the
         # blank dictionary object...
         return render(request, 'subs/login.html', {})
+
+
+
+
+# Use the login_required() decorator to ensure only those logged in can access the view.
+@login_required
+def user_logout(request):
+    # Since we know the user is logged in, we can now just log them out.
+    logout(request)
+
+    # Take the user back to the homepage.
+    return HttpResponseRedirect('/')
